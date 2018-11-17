e863b43b0d563dd91dcccb750b9366698831a435
Madhusudan Rathore <madhusudanrathore@protonmail.com>
2018-11-17 20:05:16 +0530

    1). nov 17 20:05 2). made generic implementations for previous code

---
+++ react_js/ReactCharts/src/components/Charts.js
@@ -0,0 +1,187 @@
+import React from 'react';
+import {Chart, Axis, Geom, Tooltip, Coord, Label, Legend, View, Guide, Shape, Facet} from 'bizcharts';
+import {DataSet} from '@antv/data-set';
+
+const { DataView }=DataSet;
+const { Html }=Guide;
+
+// global styles for charts
+var styles={
+  wrapper: {
+    maxWidth: 1080,
+    textAlign: 'center',
+    padding: 20,
+    marginLeft: 'auto',
+    marginRight: 'auto'
+  },
+  mainTitle: {
+    fontSize: 18,
+    color: '#333',
+    display: 'block',
+    padding: 10
+  },
+  subTitle: {
+    fontSize: 14,
+    color: '#bbb',
+    display: 'block',
+    padding: 10
+  }
+};
+
+function ConditionalTitleRender(props){
+  if( props.title_param !== "" ){
+    return(
+      <span className="main-title" style={styles.mainTitle}>
+        {props.title_param}
+      </span>
+    );
+  }else{
+    return null;
+  }
+}
+
+function ConditionalSubtitleRender(props){
+  if( props.subtitle_param !== "" ){
+    return(
+      <span className="sub-title" style={styles.subTitle}>
+        {props.subtitle_param}
+      </span>
+    );
+  }else{
+    return null;
+  }
+}
+
+class BarChart extends React.Component{
+  // below is the generic implementation for cols
+  // getScale(){
+  //     var bar_cols={
+  //       '{this.props.metadata.yAxis.name}':{
+  //         tickInterval: {this.props.metadata.tickInterval},
+  //         alias: {this.props.metadata.alias},
+  //     }
+  //   };
+  //   return bar_cols;
+  // }
+  getScale(){
+      var bar_cols={
+        'sales':{
+          tickInterval: 20,
+          alias: 'Sales (in Cr)',
+      }
+    };
+    return bar_cols;
+  }
+  render(){
+    return(
+      <div style={styles.wrapper}>
+          <Chart renderer='svg' height={this.props.metadata.height} data ={this.props.data} scale={this.getScale()} forceFit>
+
+            <ConditionalTitleRender title_param={this.props.metadata.title} />
+            <ConditionalSubtitleRender subtitle_param={this.props.metadata.subtitle} />
+            
+            <Axis name={this.props.metadata.xAxis.name} />
+            <Axis name={this.props.metadata.yAxis.name} title />
+            <Tooltip crosshairs={{type: "y"}}/>
+
+            <Geom type="interval" position={this.props.metadata.xAxis.name + "*" + this.props.metadata.yAxis.name} color={this.props.metadata.color}>
+              <Label content={this.props.geomLabel.sales.content} labelLine={this.props.geomLabel.sales.labelLine} />
+            </Geom>
+
+          </Chart>
+      </div>
+    );
+  }
+}
+
+class PieChart extends React.Component{
+
+  getScale(){
+    const cols = {
+      percent: {
+        formatter: val => {
+          val = (val.toFixed(4) * 100) + '%';
+          return val;
+        }
+      }
+    };
+    return cols;
+  }
+
+  transformData(oldData){
+     const dv1 = new DataView();
+     dv1.source(oldData).transform({
+       type: 'percent',
+       field: 'count',
+       dimension: 'item',
+       as: 'percent'
+     });
+     return dv1;
+  }
+
+  render() {
+    return <div >
+      <Chart renderer='svg' height={this.props.metadata.height} data={this.transformData(this.props.data)} scale={this.getScale()} forceFit>
+        
+        <Coord type={'theta'} radius={this.props.metadata.radius} innerRadius={this.props.metadata.innerRadius}/>
+        <Axis name="percent" />
+        <Tooltip showTitle={false}
+          itemTpl = '<li><span style="background-color:{color};" class="g2-tooltip-marker"></span>{name}: {value}</li>'
+        />
+        <Guide></Guide>
+
+        <Geom
+          type="intervalStack"
+          position="percent"
+          color ='item'
+          tooltip={['item*percent',(item, percent) => {
+            percent = percent.toFixed(4) * 100 + '%';
+            return {
+              name: item,
+              value: percent
+            };
+          }]}
+          style={{lineWidth: 1,stroke: '#fff'}}
+        >
+        <Label content='percent'  textStyle={{fontSize:24}} formatter={(val, item) => {
+            return item.point.item + '- ' + val;}} />
+        </Geom>
+
+      </Chart>
+    </div>;
+  }
+}
+
+class StackedBarChart extends React.Component{
+  transformData(){
+    const ds = new DataSet();
+    const dv = ds.createView().source(this.props.data);
+    dv.transform({
+      type: 'fold',
+      fields: this.props.metadata.elements,
+      key: 'xAxis',
+      value: 'yAxis',
+    });
+    return dv;
+  }
+  render(){
+    return(
+      <div style={styles.wrapper}>
+        <Chart height={this.props.metadata.height} data={this.transformData()} forceFit>
+        
+          <ConditionalTitleRender title_param={this.props.metadata.title} />
+          <ConditionalSubtitleRender subtitle_param={this.props.metadata.subtitle} />
+        
+          <Legend />
+          <Axis name="xAxis" />
+          <Axis name="yAxis" />
+          <Tooltip />
+          <Geom type='intervalStack' position={"xAxis*yAxis"} color={'name'} style={{stroke: '#fff',lineWidth: 1}} />
+
+        </Chart>
+      </div>
+    );
+  }
+}
+
+export {BarChart, PieChart, StackedBarChart};
--- react_js/ReactCharts/src/index.js
+++ react_js/ReactCharts/src/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import BarChart from './components/charts/Bar';
+import {BarChart, PieChart, StackedBarChart} from './components/Charts';
 
-const bardata = [
+// bar-chart-data section
+var bar_metadata={
+  height: 400,
+  color: '#a8de4d',
+  title: 'Title of Bar chart',
+  subtitle: 'Sub-Title of Bar chart',
+  xAxis: {
+    name: 'year',
+    displayTitle: false,
+    tickInterval: 20,
+    alias: 'X axis'
+  },
+  yAxis: {
+    name: 'sales',
+    displayTitle: false,
+    tickInterval: 10,
+    alias: 'Sales (in Cr)'
+  },
+};
+var bar_data=[
   { year: '1951 ', sales: 38 },
   { year: '1952 ', sales: 52 },
   { year: '1956 ', sales: 61 },
@@ -12,54 +31,45 @@ const bardata = [
   { year: '1960 ', sales: 38 },
   { year: '1962 ', sales: 38 },
 ];
-
-const metadata_array={
-  height: 400,
-  color: '#a8de4d',
-  title: '',
-  subtitle: 'Sub-Title of chart'
-};
-
-const cols = {
-  'sales': {
-    tickInterval: 20,
-    alias: 'Sales (in Cr)'
-  },
-};
-
-const geomLabel = {
+var bar_geom_label={
   sales: {
     content: ['year*sales', (year, sales) => `${sales}k`]
   }
 };
 
-const styles = {
-  wrapper: {
-    maxWidth: 1080,
-    textAlign: 'center',
-    padding: 20,
-    marginLeft: 'auto',
-    marginRight: 'auto'
-  },
-  mainTitle: {
-    fontSize: 18,
-    color: '#333',
-    display: 'block',
-    padding: 10
-  },
-  subTitle: {
-    fontSize: 14,
-    color: '#bbb',
-    display: 'block',
-    padding: 10
-  }
+// pie-chart-data section
+var pie_metadata={
+  height: 500,
+  radius: 0.75,
+  innerRadius: 0.6,
+  color: '#a8de4d',
+  title: 'Title of Pie chart',
+  subtitle: 'Sub-Title of Pie chart',
 };
+var pie_data=[
+  { item: 'abc', count: 30 },
+  { item: 'cba', count: 20 },
+  { item: 'xyz', count: 11 },
+  { item: 'zyx', count: 10 },
+  { item: 'abcagain', count: 30 }
+];
+
+// stacked-bar-chart-data section
+var stacked_bar_metadata={
+  height: 500,
+  title: 'Title of Stacked Bar chart',
+  subtitle: 'Sub-Title of Stacked Bar Chart',
+  elements: ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug']
+};
+var stacked_bar_data=[
+  { name:'London', 'Jan': 18.9, 'Feb': 28.8, 'Mar' :39.3, 'Apr': 81.4, 'May': 47, 'Jun': 20.3, 'Jul': 24, 'Aug': 35.6 },
+  { name:'Berlin', 'Jan': 12.4, 'Feb': 23.2, 'Mar' :34.5, 'Apr': 99.7, 'May': 52.6, 'Jun': 35.5, 'Jul': 37.4, 'Aug': 42.4}
+];
 
 ReactDOM.render((
   <div>
-    <BarChart metadata={metadata_array}
-      data={bardata} scale={cols} xAxis="year" yAxis="sales" styles={styles}
-      geomLabel={geomLabel}
-    />
+    <BarChart data={bar_data} metadata={bar_metadata} geomLabel={bar_geom_label} />
+    <PieChart data={pie_data} metadata={pie_metadata} />
+    <StackedBarChart data={stacked_bar_data} metadata={stacked_bar_metadata} />
   </div>
 ), document.getElementById("root"));
